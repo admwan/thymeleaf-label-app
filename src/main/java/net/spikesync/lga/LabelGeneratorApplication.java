aa085ad6246230c2ba093776279d52a2a23833c3
Fred Wan <mail@fredwan.net>
2025-04-25 14:09:11 +0200

    Completing the skeleton project with a @SpringBootApplication annotation before class LabelGeneratorApplication

--- src/main/java/net/spikesync/lga/LabelGeneratorApplication.java
+++ src/main/java/net/spikesync/lga/LabelGeneratorApplication.java
@@ -1,1 +1,11 @@
-// Placeholder for LabelGeneratorApplication.java
+package net.spikesync.lga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelGeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LabelGeneratorApplication.class, args);
+    }
+}
